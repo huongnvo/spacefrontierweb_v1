0781858ad0749033c182005e7cb45f988628e97d
huongnvo <huongnvo@uw.edu>
2015-06-29 16:00:44 -0400

    minor fixes

--- public/js/controller/attitudeController.js
+++ public/js/controller/attitudeController.js
@@ -2,18 +2,6 @@ spaceFrontierApp.controller("attitudeController", function($scope) {
     $scope.navBarSrc = "tmpl/navbar.html";
     $scope.footerSrc = "tmpl/footer.html";
 
-    $scope.totalItems = 100;
-    $scope.currentPage = 1;
-    $scope.setPage = function (pageNo) {
-        $scope.currentPage = pageNo;
-    };
-    $scope.pageChanged = function() {
-        $log.log('Page changed to: ' + $scope.currentPage);
-    };
-    $scope.maxSize = 8;
-    $scope.bigTotalItems = 72;
-    $scope.bigCurrentPage = 2;
-
     $scope.loading = false;
     $scope.myClick = function() {
         $scope.loading = true;
--- public/js/controller/toolController.js
+++ public/js/controller/toolController.js
@@ -1,4 +1,19 @@
 spaceFrontierApp.controller("toolController", function($scope) {
     $scope.navBarSrc = "tmpl/navbar.html";
     $scope.footerSrc = "tmpl/footer.html";
+
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [];
+    $scope.addSlide = function() {
+        var newWidth = 600 + slides.length + 1;
+        slides.push({
+            image: 'http://placekitten.com/' + newWidth + '/300',
+            text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+                ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+        });
+    };
+    for (var i=0; i<4; i++) {
+        $scope.addSlide();
+    }
+    
 });
