b542c51e3e443b338f50f7ca61f6d01761aace37
huongnvo <huongnvo@uw.edu>
2015-07-15 10:18:49 -0400

    add tabbing to trajectory/data

--- public/js/controller/trajectoryController.js
+++ public/js/controller/trajectoryController.js
@@ -3,6 +3,31 @@ spaceFrontierApp.controller("trajectoryController", function($scope,$http) {
     var cubesatPath = '/parts/cubesat/' + idstring;
     $scope.showDatabase = false;
 
+    $(document).ready(function() {
+        $('.nav-tabs > li > a').click(function(event){
+            event.preventDefault();//stop browser to take action for clicked anchor
+     
+            //get displaying tab content jQuery selector
+            var active_tab_selector = $('.nav-tabs > li.active > a').attr('href');                  
+     
+            //find actived navigation and remove 'active' css
+            var actived_nav = $('.nav-tabs > li.active');
+            actived_nav.removeClass('active');
+     
+            //add 'active' css into clicked navigation
+            $(this).parents('li').addClass('active');
+     
+            //hide displaying tab content
+            $(active_tab_selector).removeClass('active');
+            $(active_tab_selector).addClass('hide');
+     
+            //show target tab content
+            var target_tab_selector = $(this).attr('href');
+            $(target_tab_selector).removeClass('hide');
+            $(target_tab_selector).addClass('active');
+        });
+    });
+
     $scope.cubesat = [];
     $scope.updateData = function() {
         $http.get(cubesatPath).then(function(result) { 
